d87121f16564fb9d43abc6e6731c03bd43e972cb
Yvonne Atieno Samwel <yvonneatieno2000@gmail.com>
2025-10-23 18:54:46 +0300

    Update TaskManager.jsx

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
+import useLocalStorage from "../Hooks/useLocalStorage";
 import Button from "./Button";
 
 export default function TaskManager() {
